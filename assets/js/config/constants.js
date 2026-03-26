d4812a58466f7e534784fc387791d433f1914808
Marcela Herrera <mh460877@gmail.com>
2026-03-26 10:55:54 -0300

    Actualización de logos y estilos de tests

--- assets/js/config/constants.js
+++ assets/js/config/constants.js
@@ -2,10 +2,9 @@ export const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxw9ZQ
 export const DB_GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby6kBo-HW69iQ6sUx0YbU_xR2qsufgVLOkNCnpPBYeb0A_dn3_u3yR4Q45iK8v9FbdC/exec';
 
 
-export const URL_TEST1 = 'https://script.google.com/macros/s/AKfycbzc_th-toqUpAGh_NFTuvuSshFfg9ET4AHmwfDdtotVv4ewYvuJ1AvYeWe_s8VhnKU/exec';//DAT
-export const URL_TEST2 = 'https://script.google.com/macros/s/AKfycbwRSco0IbXv0orJzFgzw6qSESkc3uSL2ZfAWdPeH6wmHWypP0C9Xc885HCLNTjFk-y7/exec'; //KUDER
-export const URL_TEST3 = 'https://script.google.com/macros/s/AKfycbwJO2wZ0yqSbNV0Y5-ACWOza14M5uvPpeMFO_xzT2mi7woC8lhjQnU3ZfWA3G1QPcbq8Q/exec'; //IPP
-export const URL_TEST4 = 'https://script.google.com/macros/s/AKfycbwND-K16ik6l7sXjWK_IBh2elOJigMnFwer8R637hTojHZdQRMvpYhFS9iLpl4Unoh3/exec'; //WAIS-IV
-export const URL_TEST5 = 'https://script.google.com/macros/s/AKfycbzwtkqV1qjIGDgOqTnU1AVzQNg-CWPZEBESBjecshCwMUFbC6wmx3AxmeLfRwmJALgq/exec';//DOMINO48
-export const URL_TEST6 = 'https://script.google.com/macros/s/AKfycbyHp0XD_uzkROnpLZuK7VhImYe-bZ-pFTNDiTT9P67DM6_yPAbxXu9eiLw4wE0q5sBd/exec'; //BIG FIVE
-export const URL_TEST7 = 'https://script.google.com/macros/s/AKfycbz-x1sZ8Dur6m3k4U4CkOd6I4GeiwBHnVUl1GczAD95DJ3P2ESOiMEK0AWPOHkCHTNN/exec';// CHASIDE
+export const URL_TEST1 = 'https://script.google.com/macros/s/AKfycbzc_th-toqUpAGh_NFTuvuSshFfg9ET4AHmwfDdtotVv4ewYvuJ1AvYeWe_s8VhnKU/exec';//STAI
+export const URL_TEST2 = 'https://script.google.com/macros/s/AKfycbz5ZUTuUZU0Rp3QD34UbAWb5t4oR6-8RU8BNvab1rSwc5OZaaYd5B5De0YRLUQtWtJ3AA/exec'; //BIG FIVE
+export const URL_TEST3 = 'https://script.google.com/macros/s/AKfycbwJO2wZ0yqSbNV0Y5-ACWOza14M5uvPpeMFO_xzT2mi7woC8lhjQnU3ZfWA3G1QPcbq8Q/exec'; //DOMINO48
+export const URL_TEST4 = 'https://script.google.com/macros/s/AKfycbwND-K16ik6l7sXjWK_IBh2elOJigMnFwer8R637hTojHZdQRMvpYhFS9iLpl4Unoh3/exec'; //TOULOUSE
+export const URL_TEST5 = 'https://script.google.com/macros/s/AKfycbzwtkqV1qjIGDgOqTnU1AVzQNg-CWPZEBESBjecshCwMUFbC6wmx3AxmeLfRwmJALgq/exec';//ENEAGRAMA
+export const URL_TEST6 = 'https://script.google.com/macros/s/AKfycbyHp0XD_uzkROnpLZuK7VhImYe-bZ-pFTNDiTT9P67DM6_yPAbxXu9eiLw4wE0q5sBd/exec'; //DNLA
